ec8085356f394c56e1d275ed1acb2fdb2439131a
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2022-03-01 23:45:13 +0100

    Update icd10.sql
    A24

--- htdocs/cabinetmed/sql/icd10.sql
+++ htdocs/cabinetmed/sql/icd10.sql
@@ -52,6 +52,7 @@ INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 0200 , 'A2
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 0210 , 'A21'    , 'A21'    , 'Tularaemia'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 0220 , 'A22'    , 'A22'    , 'Anthrax');
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 0230 , 'A23'    , 'A23'    , 'Brucellosis');
+INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 0240 , 'A24'    , 'A24'    , 'Glanders and melioidosis');
 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 030  , 'A30-A49', 'A30-A49', 'Other bacterial diseases'); 
 
