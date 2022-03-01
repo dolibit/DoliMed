b84fb5e6bc06365b10365fa0308cf3430d50f7c6
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2022-03-01 22:29:06 +0100

    Update icd10.sql

--- htdocs/cabinetmed/sql/icd10.sql
+++ htdocs/cabinetmed/sql/icd10.sql
@@ -3,6 +3,8 @@
 -- llx_cabinetmed_diaglec 
 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, 'A00-A09', 'A00-A09', 'Intestinal infectious diseases'); 
+INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, 'A00-A09', 'A00-A09', 'Intestinal infectious diseases'); 
+INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, 'A00-A09', 'A00-A09', 'Intestinal infectious diseases'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 2, 'A15-A19', 'A15-A19', 'Tuberculosis'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 3, 'A20-A28', 'A20-A28', 'Certain zoonotic bacterial diseases'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 4, 'A30-A49', 'A30-A49', 'Other bacterial diseases'); 
