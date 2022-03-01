10befb515db566b430dce9174352999b03808b9a
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2022-03-01 22:20:20 +0100

    Update icd10.sql

--- htdocs/cabinetmed/sql/icd10.sql
+++ htdocs/cabinetmed/sql/icd10.sql
@@ -1,5 +1,11 @@
 -- ICD-10 
 
--- llx_cabinetmed_diaglec
+-- llx_cabinetmed_diaglec 
 
-INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, U99.9!, 'Other', 'Other');
+INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, A00-A09, 'Other', 'Other'); 
+INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, U99.9!, 'Other', 'Other'); 
+INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, U99.9!, 'Other', 'Other'); 
+INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, U99.9!, 'Other', 'Other'); 
+INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, U99.9!, 'Other', 'Other'); 
+INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, U99.9!, 'Other', 'Other'); 
+INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, U99.9!, 'Other', 'Other'); 
