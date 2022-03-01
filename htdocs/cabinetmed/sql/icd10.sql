32edde749b13908b339ac4b1023db0c17bbe999f
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2022-03-01 22:35:15 +0100

    Update icd10.sql

--- htdocs/cabinetmed/sql/icd10.sql
+++ htdocs/cabinetmed/sql/icd10.sql
@@ -8,7 +8,7 @@ INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES (  1, 'A00.0
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES (  1, 'A00.1'  , 'A00.1'  , 'Cholera eltor'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES (  1, 'A00.9'  , 'A00.9'  , 'Cholera, unspecified'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES (  1, 'A01'    , 'A01'    , 'Typhoid and paratyphoid fevers'); 
-INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES (  1, 'A00'    , 'A00'    , 'Cholera'); 
+INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES (  1, 'A02'    , 'A02'    , 'Other salmonella infections'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES (  1, 'A00'    , 'A00'    , 'Cholera'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES (  1, 'A00'    , 'A00'    , 'Cholera'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES (  2, 'A15-A19', 'A15-A19', 'Tuberculosis'); 
