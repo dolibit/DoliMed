41beecc1d9b5b1b2ba4166827422c19a9a7c43f7
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2022-03-01 22:24:11 +0100

    Update icd10.sql

--- htdocs/cabinetmed/sql/icd10.sql
+++ htdocs/cabinetmed/sql/icd10.sql
@@ -2,10 +2,10 @@
 
 -- llx_cabinetmed_diaglec 
 
-INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, A00-A09, 'Other', 'Other'); 
+INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, 'A00-A09', 'A00-A09', 'Intestinal infectious diseases'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, U99.9!, 'Other', 'Other'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, U99.9!, 'Other', 'Other'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, U99.9!, 'Other', 'Other'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, U99.9!, 'Other', 'Other'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, U99.9!, 'Other', 'Other'); 
-INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, U99.9!, 'Other', 'Other'); 
+INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, 'U99.9!', 'Other', 'Other'); 
