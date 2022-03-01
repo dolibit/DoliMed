21fff932e0e3a4ceb36c6b4e161716044320b3c3
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2022-03-01 23:20:55 +0100

    Update icd10.sql

--- htdocs/cabinetmed/sql/icd10.sql
+++ htdocs/cabinetmed/sql/icd10.sql
@@ -26,8 +26,8 @@ INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 21  , 'Z00
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 22  , 'U00-U85', 'Chapter XXII'  , 'Codes for special purposes');
 
 
-INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 001  , 'A00-A09', 'A00-A09'   , 'Intestinal infectious diseases'); 
-INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 010  , 'A00'    , 'A00'    , 'Cholera'); 
+INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 010  , 'A00-A09', 'A00-A09'   , 'Intestinal infectious diseases'); 
+INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 011  , 'A00'    , 'A00'    , 'Cholera'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 0100 , 'A00.0'  , 'A00.0'  , 'Cholera due to Vibrio cholerae 01, biovar cholerae'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 01001, 'A00.1'  , 'A00.1'  , 'Cholera eltor'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 01009, 'A00.9'  , 'A00.9'  , 'Cholera, unspecified'); 
@@ -40,9 +40,11 @@ INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 0106 , 'A0
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 0107 , 'A07'    , 'A07'    , 'Other bacterial intestinal infections'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 0108 , 'A08'    , 'A08'    , 'Other bacterial intestinal infections'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 0109 , 'A09'    , 'A09'    , 'Other gastroenteritis and colitis of infectious and unspecified origin'); 
+
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES (  2, 'A15-A19', 'A15-A19', 'Tuberculosis'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES (  3, 'A20-A28', 'A20-A28', 'Certain zoonotic bacterial diseases'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES (  4, 'A30-A49', 'A30-A49', 'Other bacterial diseases'); 
+
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES (  1, U99.9!, 'Other', 'Other'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES (  1, U99.9!, 'Other', 'Other'); 
-INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES (  1, 'U99.9!', 'Other', 'Other'); 
+INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 2209, 'U99.9!', 'Other', 'Other'); 
