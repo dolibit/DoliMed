5a17e03684adaa515bff84151fd8fb3f897271c4
UT from dolibit <45215329+dolibit-ut@users.noreply.github.com>
2022-03-01 22:29:58 +0100

    Update icd10.sql

--- htdocs/cabinetmed/sql/icd10.sql
+++ htdocs/cabinetmed/sql/icd10.sql
@@ -3,7 +3,7 @@
 -- llx_cabinetmed_diaglec 
 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, 'A00-A09', 'A00-A09', 'Intestinal infectious diseases'); 
-INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, 'A00-A09', 'A00-A09', 'Intestinal infectious diseases'); 
+INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, 'A00'    , 'A00'    , 'Cholera'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 1, 'A00-A09', 'A00-A09', 'Intestinal infectious diseases'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 2, 'A15-A19', 'A15-A19', 'Tuberculosis'); 
 INSERT INTO llx_cabinetmed_diaglec (rowid, icd, code, label) VALUES ( 3, 'A20-A28', 'A20-A28', 'Certain zoonotic bacterial diseases'); 
